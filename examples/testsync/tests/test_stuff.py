a349fcea9186952d3aa1b78d3ecffeadb1a4dc92
Wouter De Borger <wouter.deborger@inmanta.com>
2019-02-24 21:03:30 +0100

    Issue/mockagent broken (#10)
    * fix for run_sync changes in inmanta#949

---
+++ examples/testsync/tests/test_stuff.py
@@ -0,0 +1,16 @@
+"""
+    Copyright 2018 Inmanta
+    Contact: code@inmanta.com
+    License: Apache 2.0
+"""
+
+def test_basic_run(project):
+    basemodel = """
+    import testsync
+
+    r = testsync::Resource(agent="a", name="IT", key="k", value="write")
+    """
+
+    project.compile(basemodel)
+
+    project.deploy_resource("testsync::Resource")
