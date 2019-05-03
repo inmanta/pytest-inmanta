eb03dfe66c220f70f6455320a4b0d2c8feab61c8
Wouter De Borger <wouter.deborger@inmanta.com>
2019-05-03 10:18:22 +0200

    added dryrun
    fixes #3

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# V 1.0.0
+Changes in this release:
+- added log serialization to deploy, to better mimic agent behavior
+- added dryrun 
--- pytest_inmanta/plugin.py
+++ pytest_inmanta/plugin.py
@@ -254,9 +254,11 @@ class Project():
         assert h is not None
 
         ctx = handler.HandlerContext(resource)
-        h.execute(ctx, resource, False)
-
+        h.execute(ctx, resource, dry_run)
         return ctx
+
+    def dryrun(self, resource, run_as_root=False):
+        return self.deploy(resource, True, run_as_root)
 
     def deploy_resource(self, resource_type: str, status=const.ResourceState.deployed, run_as_root=False, **filter_args: dict):
         res = self.get_resource(resource_type, **filter_args)
@@ -274,6 +276,15 @@ class Project():
 
         assert ctx.status == status
         return res
+
+    def dryrun_resource(self, resource_type: str, status=const.ResourceState.deployed, run_as_root=False,
+                        **filter_args: dict):
+        res = self.get_resource(resource_type, **filter_args)
+        assert res is not None, "No resource found of given type and filter args"
+
+        ctx = self.dryrun(res, run_as_root)
+        assert ctx.status == const.ResourceState.dry
+        return ctx.changes
 
     def io(self, run_as_root=False):
         version = 1
