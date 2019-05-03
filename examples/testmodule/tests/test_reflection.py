72239cb500d245f8b58ba2efda863bd67422572f
Wouter De Borger <wouter.deborger@inmanta.com>
2019-05-03 11:57:16 +0200

    added improved model reflection (#15)

---
+++ examples/testmodule/tests/test_reflection.py
@@ -0,0 +1,50 @@
+"""
+    Copyright 2018 Inmanta
+    Contact: code@inmanta.com
+    License: Apache 2.0
+"""
+import inmanta
+import pytest
+
+
+def test_model_inspection(project):
+    basemodel = """
+    import testmodule
+
+    r = testmodule::Resource(agent="a", name="r", key="k", value="write")
+    r2 = testmodule::Resource(agent="a", name="r2", key="k", value="write")
+    
+    entity Other:
+        string name
+    end
+    
+    implement Other using std::none
+    
+    Other.resource [0:] -- testmodule::Resource
+    Other.optional [0:1] -- testmodule::Resource
+    
+    a = Other(name="a")
+    b = Other(name="b")
+    
+    a.resource = r
+    a.resource = r2
+    """
+
+    project.compile(basemodel)
+
+    all = project.get_instances()
+
+    named = {a.name: a for a in all}
+
+    ar = named["a"].resource
+    assert len(ar) == 2
+    namesinar = [a.name for a in ar]
+    assert sorted(namesinar) == ["r", "r2"]
+
+    with pytest.raises(inmanta.ast.NotFoundException):
+        named["a"].xx
+
+    assert not named["b"].resource
+
+    with pytest.raises(inmanta.ast.OptionalValueException):
+        named["a"].optional
--- tests/test_basic_example.py
+++ tests/test_basic_example.py
@@ -28,6 +28,16 @@ def test_run_sync(testdir):
     result.assert_outcomes(passed=1)
 
 
+def test_run_reflection(testdir):
+    """Make sure that the run_sync mock works."""
+
+    testdir.copy_example("testmodule")
+
+    result = testdir.runpytest("tests/test_reflection.py")
+
+    result.assert_outcomes(passed=1)
+
+
 def test_run_capture(testdir):
     """Make sure that the run_sync mock works."""
 
