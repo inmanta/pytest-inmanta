aae830b0f0b462334352fedb0707af335a6c4c35
Yaron Vanhulst <kazaamjt@gmail.com>
2020-01-23 17:18:03 +0100

    Simplify test

--- tests/test_options.py
+++ tests/test_options.py
@@ -6,20 +6,22 @@ import pytest_inmanta
 def test_module_in_place(testdir):
     """Make sure the run in place option works"""
 
+    # copy_example copies what is IN the given directory, not the directory itself...
+    testdir.copy_example("testmodule")
     # Moving module to make sure we can run in place,
     # by making sure the module name in module.yml is in it's parent path
-    testdir.copy_example("testmodule_2")
     os.mkdir("testmodule")
     shutil.move("model", "testmodule/model")
     shutil.move("module.yml", "testmodule/module.yml")
     shutil.move("plugins", "testmodule/plugins")
     shutil.move("tests", "testmodule/tests")
+
     os.chdir("testmodule")
     path = os.getcwd()
     assert not os.path.exists(os.path.join(path, "testfile"))
     pytest_inmanta.plugin.CURDIR = (path)
 
-    result = testdir.runpytest("tests/test_resource_run_2.py", "--use-module-in-place")
+    result = testdir.runpytest("tests/test_location.py", "--use-module-in-place")
 
     result.assert_outcomes(passed=1)
 
