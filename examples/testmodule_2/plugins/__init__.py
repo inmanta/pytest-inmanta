5c3d5900eb1f28ca256f64128acd2d3e88bc9ff3
Yaron Vanhulst <kazaamjt@gmail.com>
2020-01-23 13:06:36 +0100

    Add test to make sure we are in place

---
+++ examples/testmodule_2/plugins/__init__.py
@@ -0,0 +1,47 @@
+"""
+    Copyright 2018 Inmanta
+    Contact: code@inmanta.com
+    License: Apache 2.0
+"""
+import os
+from pathlib import Path
+
+from inmanta.agent.handler import provider, CRUDHandler, HandlerContext
+from inmanta.resources import resource, PurgeableResource
+
+@resource("testmodule::Resource", agent="agent", id_attribute="name")
+@resource("testmodule::BadLog", agent="agent", id_attribute="name")
+class ResourceResource(PurgeableResource):
+    fields = ("name", "agent", "key", "value")
+
+
+@provider("testmodule::Resource", name="resourceprovider")
+class ResourceHandler(CRUDHandler):
+
+    def read_resource(self, ctx: HandlerContext, resource: ResourceResource) -> None:
+        resource.purged = False
+        resource.value = "read"
+        # This is to verify we are where we say we are
+        Path(os.path.realpath(__file__)).parent.parent.joinpath("testfile").touch()
+
+    def create_resource(self, ctx: HandlerContext, resource: ResourceResource) -> None:
+        ctx.set_created()
+
+    def update_resource(self, ctx: HandlerContext, changes: dict, resource: ResourceResource) -> None:
+        ctx.set_updated()
+
+    def facts(self, ctx, resource):
+        facts = {"fact": "fact"}
+        return facts
+
+    def delete_resource(self, ctx: HandlerContext, resource: ResourceResource) -> None:
+        ctx.set_purged()
+
+
+@provider("testmodule::BadLog", name="BadLog")
+class ResourceHandler(ResourceHandler):
+
+    def read_resource(self, ctx: HandlerContext, resource: ResourceResource) -> None:
+        resource.purged = False
+        resource.value = "read"
+        ctx.warning("argument can not be serialized", argument={"a":"b"}.values())
--- tests/test_options.py
+++ tests/test_options.py
@@ -8,15 +8,19 @@ def test_module_in_place(testdir):
 
     # Moving module to make sure we can run in place,
     # by making sure the module name in module.yml is in it's parent path
-    testdir.copy_example("testmodule")
+    testdir.copy_example("testmodule_2")
     os.mkdir("testmodule")
     shutil.move("model", "testmodule/model")
     shutil.move("module.yml", "testmodule/module.yml")
     shutil.move("plugins", "testmodule/plugins")
     shutil.move("tests", "testmodule/tests")
     os.chdir("testmodule")
-    pytest_inmanta.plugin.CURDIR = (os.getcwd())
+    path = os.getcwd()
+    assert not os.path.exists(os.path.join(path, "testfile"))
+    pytest_inmanta.plugin.CURDIR = (path)
 
-    result = testdir.runpytest("tests/test_resource_run.py", "--use-module-in-place")
+    result = testdir.runpytest("tests/test_resource_run_2.py", "--use-module-in-place")
 
     result.assert_outcomes(passed=1)
+
+    assert os.path.exists(os.path.join(path, "testfile"))
