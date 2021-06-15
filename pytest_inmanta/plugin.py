b7738d488ccdad8f47b4238d05075490459f4444
Arnaud Schoonjans <arnaud.schoonjans@inmanta.com>
2021-06-15 10:56:41 +0200

    Set no_dedent option of compile() to True by default for backward compatibility

--- pytest_inmanta/plugin.py
+++ pytest_inmanta/plugin.py
@@ -621,7 +621,7 @@ license: Test License
         module.Project.set(test_project)
         test_project.load()
 
-    def compile(self, main: str, export: bool = False, no_dedent: bool = False) -> None:
+    def compile(self, main: str, export: bool = False, no_dedent: bool = True) -> None:
         """
         Compile the configuration model in main. This method will load all required modules.
 
