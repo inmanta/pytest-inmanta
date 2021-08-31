4be7bee2c304c04631389653608b3bae2751828b
Sander Van Balen <git@sandervanbalen.be>
2021-08-31 10:41:16 +0200

    added comment

--- examples/testmodule/tests/test_multiple_repo_paths.py
+++ examples/testmodule/tests/test_multiple_repo_paths.py
@@ -13,7 +13,9 @@ def test_multiple_paths(project):
     project_inmanta = module.Project(project._test_project_dir)
     children: List[module.ModuleRepo] = (
         project_inmanta.module_source_v1.remote_repo.children
+        # latest inmanta-core
         if hasattr(project_inmanta, "module_source_v1")
+        # backwards compatibility with ISO3 and ISO4
         else project_inmanta.externalResolver.children
     )
     module_repo_urls = [repo.baseurl for repo in children]
