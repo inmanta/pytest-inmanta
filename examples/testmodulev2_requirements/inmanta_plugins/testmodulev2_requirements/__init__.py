745ab4f859c22732204e1d869fe5109acb88a783
Lejoly Florent <florent.lejoly@inmanta.com>
2022-07-18 16:57:29 +0200

    update

---
+++ examples/testmodulev2_requirements/inmanta_plugins/testmodulev2_requirements/__init__.py
@@ -0,0 +1,11 @@
+"""
+    Copyright 2021 Inmanta
+    Contact: code@inmanta.com
+    License: Apache 2.0
+"""
+from inmanta.plugins import plugin
+
+
+@plugin
+def myplugin(x: "int") -> "int":
+    return x
