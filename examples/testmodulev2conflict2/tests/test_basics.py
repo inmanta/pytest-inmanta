cc20e4c5eca0e04a820536987c22599570f193e0
Lejoly Florent <florent.lejoly@inmanta.com>
2022-07-20 11:12:18 +0200

    update

---
+++ examples/testmodulev2conflict2/tests/test_basics.py
@@ -0,0 +1,12 @@
+"""
+    Copyright 2021 Inmanta
+    Contact: code@inmanta.com
+    License: Apache 2.0
+"""
+
+
+def test_compile(project):
+    """
+    Verify that basic compilation using the project fixture works for v2 modules.
+    """
+    project.compile("import testmodule-v2-conflict-2")
