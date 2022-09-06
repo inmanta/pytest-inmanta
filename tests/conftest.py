b27514aabfe2b785895686b0eaca5c2391a1daf9
Hugo-Inmanta <98876549+Hugo-Inmanta@users.noreply.github.com>
2022-09-06 13:36:55 +0200

    Issue/318 port venv cleanup fixture fixes from core (#326)
    * [FEAT] cleanup sys.meta_path between tests
    
    * [FEAT] port venv cleanup fixture fixes from core
    
    * [FIX] perform requested changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,6 +46,8 @@ def deactive_venv():
     old_os_path = os.environ.get("PATH", "")
     old_prefix = sys.prefix
     old_path = sys.path
+    old_meta_path = sys.meta_path.copy()
+    old_path_hooks = sys.path_hooks.copy()
     old_pythonpath = os.environ.get("PYTHONPATH", None)
     old_os_venv: Optional[str] = os.environ.get("VIRTUAL_ENV", None)
     old_working_set = pkg_resources.working_set
@@ -55,6 +57,13 @@ def deactive_venv():
     os.environ["PATH"] = old_os_path
     sys.prefix = old_prefix
     sys.path = old_path
+    # reset sys.meta_path because it might contain finders for editable installs, make sure to keep the same object
+    sys.meta_path.clear()
+    sys.meta_path.extend(old_meta_path)
+    sys.path_hooks.clear()
+    sys.path_hooks.extend(old_path_hooks)
+    # Clear cache for sys.path_hooks
+    sys.path_importer_cache.clear()
     pkg_resources.working_set = old_working_set
     # Restore PYTHONPATH
     if old_pythonpath is not None:
