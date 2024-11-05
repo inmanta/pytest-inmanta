bf5e2986546afbbda85262147a62de16daf9a35e
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2024-11-05 10:02:15 +0100

    Use pydantic v2 if available

--- pytest_inmanta/plugin.py
+++ pytest_inmanta/plugin.py
@@ -258,7 +258,12 @@ def get_project_repos(repo_options: typing.Sequence[str]) -> typing.Sequence[obj
                             + alternative_text,
                             parameters.pip_index_url.environment_variable,
                         )
-            return json.loads(repo_info.json())
+
+            try:
+                return repo_info.model_dump(mode="json")
+            except AttributeError:
+                # Backward compatibility with pydantic v1
+                return json.loads(repo_info.json())
 
     return [parse_repo(repo) for repo in repo_options]
 
@@ -413,9 +418,13 @@ def project_factory(
             raise
 
     with open(os.path.join(project_dir, "project.yml"), "w+") as fd:
-        # pydantic.BaseModel.dict() doesn't produce data that can be serialized
-        # so we first serialize it as json, then load it and dump it as yaml.
-        yaml.safe_dump(json.loads(project_metadata.json()), fd)
+        try:
+            yaml.safe_dump(project_metadata.model_dump(mode="json"), fd)
+        except AttributeError:
+            # Backward compatibility with pydantic v1
+            # pydantic.BaseModel.dict() doesn't produce data that can be serialized
+            # so we first serialize it as json, then load it and dump it as yaml.
+            yaml.safe_dump(json.loads(project_metadata.json()), fd)
 
     ensure_current_module_install(
         os.path.join(project_dir, "libs"),
